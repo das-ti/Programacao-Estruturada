d6dea33497f7e026196690d00b8ea7e39ba7b2e0
Douglas Alves <89371510+das-ti@users.noreply.github.com>
2025-08-15 01:00:04 -0300

    Update atv1-08.java

--- atv1-08.java
+++ atv1-08.java
@@ -10,20 +10,20 @@ public class Main {
 
 		System.out.print("Digite um inteiro: ");
 		n = entrada.nextInt();
-
+		
 		if (n < 0) {
 			System.out.println("O número não pode ser negativo.");
 		} 
 		else {
+
 			int soma = 0;
-			int count = 0;
-			int atual = 1;
-			while (count < n) {
-				soma += atual;
-				count++;
-				atual += 2;
-				System.out.println("A soma dos primeiros " + n + " números ímpares é: " + soma);
+			int impar = 1;
+			for (int count = 0; count < n; count++) {
+				soma += impar;
+				impar += 2;
 			}
+			System.out.println("A soma dos primeiros " + n + " números ímpares é: " + soma);
+
 			entrada.close();
 		}
 	}
